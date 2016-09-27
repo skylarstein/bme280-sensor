3baf2aefad956d2542ec3064dc4827733889843b
skylarstein <skylar@peringer.com>
2016-09-27 21:36:32 +0000

    using string template literals in example.js

--- example.js
+++ example.js
@@ -8,24 +8,24 @@ const bme280 = new BME280();
 const options = { i2cBusNo : 1, i2cAddress : BME280.BME280_DEFAULT_I2C_ADDRESS() };
 
 bme280.init(options)
-  .then((result) => {
-     console.log('BME280 initialization succeeded');
+  .then(result => {
+     console.log(`BME280 initialization succeeded`);
      readSensorData();
   })
-  .catch((err) => console.error('BME280 initialization failed: ' + err));
+  .catch(err => console.error(`BME280 initialization failed: ${errr} `));
 
 const readSensorData = () => {
   bme280.readSensorData()
-    .then((data) => {
+    .then(data => {
       // temperature_C, pressure_hPa, and humidity are returned by default.
       // I'll also calculate some unit conversions for display purposes.
       //
       data['temperature_F'] = BME280.convertCelciusToFahrenheit(data.temperature_C);
       data['pressure_inHg'] = BME280.convertHectopascalToInchesOfMercury(data.pressure_hPa);
  
-      console.log('data = ' + JSON.stringify(data, null, 2));
+      console.log(`data = ${JSON.stringify(data, null, 2)}`);
     })
-    .catch((err) => console.log('BME280 read error: ' + err))
+    .catch(err => console.log(`BME280 read error: ${err}`))
 
   setTimeout(readSensorData, 2000);
 }
